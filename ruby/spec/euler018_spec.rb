0f3320fad58f75b842c959b60d15fd6b2164738e
Tim Varley <tim.varley@gmail.com>
2021-05-25 14:16:05 -0400

    Ruby 18

---
+++ ruby/spec/euler018_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'euler018'
+require 'byebug'
+
+describe 'Euler 018 solution' do
+  describe 'maximum_path_sum_one' do
+    it 'should calculate euler 018 example correctly' do
+      expect(maximum_path_sum_one('./lib/euler_18_test_data.txt')).to eq 23
+    end
+
+    it 'should calculate euler 018 correctly' do
+      expect(maximum_path_sum_one('./lib/euler_18_data.txt')).to eq 1074
+    end
+  end
+end
