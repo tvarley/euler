e9545dc6405df1d21f2091c32d61cd64c80a0455
Tim Varley <tim.varley@gmail.com>
2021-05-27 16:53:32 -0400

    c++ 23 tweaks

--- cpp/src/test_prob_023.h
+++ cpp/src/test_prob_023.h
@@ -1,19 +1,19 @@
 #include "euler023.cpp"
 
 namespace{
+  TEST(Prob023,IsPerfect){
+    EXPECT_EQ(PERFECT,how_perfect(28));
+  }
+
+  TEST(Prob023,IsDeficient){
+    EXPECT_EQ(ABUNDENT,how_perfect(26));
+  }
+
+  TEST(Prob023,IsAbundent){
+    EXPECT_EQ(ABUNDENT,how_perfect(29));
+  }
+
   TEST(Prob023,Answer){
-    EXPECT_EQ(4179871,non_abundunt_sums(12));
-  }
-  // TEST(Prob023,SumF001){
-  //   EXPECT_EQ(16,sum_factors(12));
-  // }
-  // TEST(Prob023,HowPerfect001){
-  //   EXPECT_EQ(ABUNDENT,how_perfect(12));
-  // }
-  TEST(Prob023,HowPerfect002){
-    EXPECT_EQ(PERFECT,how_perfect(19));
-  }
-  TEST(Prob023,HowPerfect003){
-    EXPECT_EQ(PERFECT,how_perfect(6));
+    EXPECT_EQ(4179871,non_abundunt_sums());
   }
 };
