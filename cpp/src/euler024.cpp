ccdb0bbc7544fe13b4b226f9d5d561eb72d0f412
Tim Varley <tim.varley@gmail.com>
2021-06-02 19:20:39 -0400

    C++ 24 tweaks

--- cpp/src/euler024.cpp
+++ cpp/src/euler024.cpp
@@ -22,7 +22,8 @@
 std::string lexicographic_permutations_cheat(std::string input)
 {
   int perm_count = 0;
-  std::string result; 
+  std::string result;
+  std::sort(input.begin(), input.end());
   do {
     result = input;
   } while(std::next_permutation(input.begin(), input.end()) && ++perm_count < 1000000);
@@ -30,18 +31,32 @@ std::string lexicographic_permutations_cheat(std::string input)
   return result;
 }
 
-uint64_t factorial(uint64_t num)
+// @see - https://www.geeksforgeeks.org/lexicographic-permutations-of-string/
+// Following are the steps to print the permutations lexicographic-ally
+// 1. Sort the given string in non-decreasing order and print it. 
+// The first permutation is always the string sorted in non-decreasing order.
+// 2. Start generating next higher permutation. 
+// Do it until next higher permutation is not possible. 
+// If we reach a permutation where all characters are sorted in non-increasing order, 
+// then that permutation is the last permutation.
+// 
+// Steps to generate the next higher permutation:
+// 1. Take the previously printed permutation and find the rightmost character in it, 
+// which is smaller than its next character. Let us call this character as ‘first character’.
+// 2. Now find the ceiling of the ‘first character’. 
+// Ceiling is the smallest character on right of ‘first character’, 
+// which is greater than ‘first character’. Let us call the ceil character as ‘second character’.
+// 3. Swap the two characters found in above 2 steps.
+// 4. Sort the substring (in non-decreasing order) after the original index of ‘first character’.
+std::string lexicographic_permutations(const std::string& input)
 {
-  uint64_t result = 1;
-  for(uint64_t i = 1; i <= num; i++) {
-    result *= i;
-  }
-  return result;
-}
-
-std::string lexicographic_permutations(std::string input)
-{
+  std::string result = input;
+  std::sort(result.begin(), result.end());
   std::cout << "Input: " << input << std::endl;
+  std::cout << "Result: " << result << std::endl;
+  int perm_count = 0;
+  while( ++perm_count < 1000000 ) {
+  }
   return input;
 }
 
@@ -49,28 +64,30 @@ std::string lexicographic_permutations(std::string input)
 int main(int argc, char const *argv[])
 {
   std::string solution = "2783915460";
-  std::string digits("0123456789");
+  // std::string digits("0123456789");
+  std::string digits("0132456789");
   std::cout << "Solution: " << solution << std::endl;
 
+  // ------8<---- Cheat Mode---8<-------
   {
     simple_timer x("Lexicographics permutations (cheat mode)");
+    std::string cheat_answer = lexicographic_permutations_cheat(digits);
+    std::cout << "Cheat mode" << std::endl;
+    std::cout << "Answer: " << cheat_answer << std::endl;
+    std::cout << "Correct?: " << (cheat_answer == solution ? "PASS" : "FAIL") << std::endl;
   }
+  // ------8<---- Cheat Mode---8<-------
 
-  std::chrono::high_resolution_clock hr_clock;
-  // ------8<----Cheat mode-------8<-------
-  std::cout << "Cheat mode" << std::endl;
-  auto start_time = hr_clock.now();
-  std::string my_answer = lexicographic_permutations_cheat(digits);
-  auto end_time = hr_clock.now();
-  std::cout << "Answer: " << my_answer << std::endl;
-  auto duration = end_time-start_time;
-  std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << std::endl;
-  std::cout << "Correct?: " << (my_answer == solution ? "PASS" : "FAIL") << std::endl;
-  // ------8<----Cheat mode-------8<-------
-  
   // ------8<----Non Cheat Mode---8<-------
-  std::cout << "Factorial test: " << factorial(5) << std::endl;
+  {
+    simple_timer x("Lexicographics permutations ( non cheat mode)");
+    std::string answer = lexicographic_permutations(digits);
+    std::cout << "Cheat mode" << std::endl;
+    std::cout << "Answer: " << answer << std::endl;
+    std::cout << "Correct?: " << (answer == solution ? "PASS" : "FAIL") << std::endl;
+  }
   // ------8<----Non Cheat Mode---8<-------
+
 
   // std::cout << "Answer: " << lexicographic_permutations(digits) << std::endl;
 }
